653a676b5de03247671a7ab66ac21a9b5bf56aeb
Débora Macêdo <deboramacedo886@gmail.com>
2024-01-07 20:03:25 -0300

    desafio de Poo

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,24 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string IMEI {get; set;}
 
-        public Smartphone(string numero)
+        private string Modelo {get; set;}
+
+        private int Memoria {get; set;}
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria= memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
+        }
+
+        protected Smartphone(string numero)
+        {
+            Numero = numero;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+System.Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123321", modelo: "Modelo 1", imei: "12344321", memoria:64 );
+nokia.Ligar();
+nokia.InstalarAplicativo("whats");
+
+System.Console.WriteLine("\n");
+
+System.Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "12332134", modelo: "Modelo 13", imei: "1234432123", memoria:128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("telegram");
+
+
